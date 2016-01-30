46ee94259d556c554f5b928302f9d9d8e85c21e6
silent-hero <p.jazauskas@gmail.com>
2016-01-30 21:39:45 +0200

    added intro

---
+++ Zabuton/Assets/_Scripts/IntroRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroRotation : MonoBehaviour 
+{
+    private float currentRotationY = 0f;
+    private float currentRotationZ = 0f;
+
+	void Update () 
+    {
+        currentRotationY += Time.deltaTime;
+        currentRotationZ += Time.deltaTime * 2;
+
+        gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, currentRotationY * 2, currentRotationZ);
+
+	}
+}
---
+++ Zabuton/Assets/_Scripts/SkipIntro.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkipIntro : MonoBehaviour 
+{
+
+	void Update () 
+    {
+
+	    if(Input.anyKey)
+        {
+            Application.LoadLevel(1);
+        }
+	}
+
+}
