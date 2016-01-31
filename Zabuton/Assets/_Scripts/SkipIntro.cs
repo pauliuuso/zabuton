f2825944d88f31e120310d6c0a9a2824ee236910
silent-hero <p.jazauskas@gmail.com>
2016-01-31 22:07:14 +0200

    added loading splash screen

--- Zabuton/Assets/_Scripts/SkipIntro.cs
+++ Zabuton/Assets/_Scripts/SkipIntro.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SkipIntro : MonoBehaviour 
 {
+    public GameObject splashScreen;
 
 	void Update () 
     {
 
 	    if(Input.anyKey)
         {
+            gameObject.GetComponent<AudioSource>().enabled = false;
+            splashScreen.SetActive(true);
             Application.LoadLevel(1);
         }
 	}
